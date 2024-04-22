16354e8d6bfbdc78756224eb8dd6ac9532454c49
Borodi21 <Borod_yi21@student.itstep.org>
2024-04-23 00:42:53 +0300

    add  input for last name
    Add  input for last name, output for last name is magenta color.

--- testITStep/Program.cs
+++ testITStep/Program.cs
@@ -16,8 +16,13 @@ namespace testITStep
             Console.Write("Enter middle name: ");
             string middle_name = Console.ReadLine();
 
+
+            Console.Write("Enter last name: ");
+            string last_name = Console.ReadLine();
+
             ConsoleColor name_color = ConsoleColor.Green;
             ConsoleColor middle_name_color = ConsoleColor.Blue;
+            ConsoleColor last_name_color = ConsoleColor.Magenta;
 
             Console.ForegroundColor = name_color;
             Console.Write(name);
@@ -25,6 +30,10 @@ namespace testITStep
             Console.ForegroundColor = middle_name_color;
             Console.Write(" ");
             Console.Write(middle_name);
+
+            Console.ForegroundColor = last_name_color;
+            Console.Write(" ");
+            Console.WriteLine(last_name);
 
             Console.ResetColor();
         }
